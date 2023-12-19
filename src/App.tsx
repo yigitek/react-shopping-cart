91374b0d2127abd2435fddd4db76b95101b8cb6a
yigitek <yigitek@gmail.com>
2023-12-19 05:06:02 +0100

    added hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,19 @@ import { Home } from "./pages/Home"
 import { About } from "./pages/About"
 import { Store } from "./pages/Store"
 import { Navbar } from "./components/Navbar"
+import { ShoppingCartProvider } from "./context/ShoppingCartContext"
 function App() {
   return (
-    <>
-  <Navbar />
-  <Container className="mb-4">
-    <Routes>
-      <Route path="/" element={<Home />}/>
-      <Route path="/store" element={<Store />}/>
-      <Route path="/about" element={<About />}/>
-    </Routes>
-
-  </Container>
-  </>
+  <ShoppingCartProvider>
+    <Navbar />
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/store" element={<Store />}/>
+        <Route path="/about" element={<About />}/>
+      </Routes>
+    </Container>
+  </ShoppingCartProvider>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,9 @@
 import {Button, Container, Nav, Navbar as NavbarBs} from "react-bootstrap"
 import { NavLink } from "react-router-dom"
+import { useShoppingCart } from "../context/ShoppingCartContext"
 
 export function Navbar(){
+    const { openCart, cartQuantity} = useShoppingCart()
     return (
     <NavbarBs sticky="top" className="bg-gray shadow-sm mb-3">
         <Container>
@@ -16,25 +18,28 @@ export function Navbar(){
                     Store
                 </Nav.Link>
             </Nav>
-            <Button style={{width:"3rem", height:"3rem", position:"relative"}}
+            {cartQuantity> 0 && (<Button 
+            onClick={openCart}
+            style={{width:"3rem", height:"3rem", position:"relative"}}
             variant="outline-primary"
             className="rounded-circle">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm1.336-5l1.977-7h-16.813l2.938 7h11.898zm4.969-10l-3.432 12h-12.597l.839 2h13.239l3.474-12h1.929l.743-2h-4.195z"/></svg>
-            <div className="rounded-circle bg-danger d-flex justify-content-center
-            align-items-center" 
-            style={{
-                color:"white", 
-                width:"1.5rem", 
-                height:"1.5rem",
-                position:"absolute", 
-                bottom: 0, 
-                right:0, 
-                transform:"translate(25%, 25%)",
-            }}
-        >
-                3
-             </div>
-            </Button>
+                <div 
+                className="rounded-circle bg-danger d-flex justify-content-center
+                align-items-center" 
+                style={{
+                    color:"white", 
+                    width:"1.5rem", 
+                    height:"1.5rem",
+                    position:"absolute", 
+                    bottom: 0, 
+                    right:0, 
+                    transform:"translate(25%, 25%)",
+                }}
+            >
+                    {cartQuantity}
+                </div>
+            </Button>)}
         </Container>
     </NavbarBs>
     )
